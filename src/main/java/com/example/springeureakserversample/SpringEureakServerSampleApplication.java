1d6cc1e16fb88ba6ef35ba914472f7f1a824f9f6
ankit <ankitforstudy@gmail.com>
2021-07-10 12:44:09 +0530

    Initial commit

---
+++ src/main/java/com/example/springeureakserversample/SpringEureakServerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springeureakserversample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEureakServerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEureakServerSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springeureakserversample/SpringEureakServerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springeureakserversample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEureakServerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
